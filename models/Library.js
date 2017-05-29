4c196200cf8f3f7b9498b9425da8fc23448dcfce
mochacat <cat.r.mocha@gmail.com>
2017-05-29 05:42:13 -0700

    added library schema and set up express

---
+++ models/Library.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+
+var librarySchema = new mongoose.Schema({
+  _id : { type: String , unique: true },
+  name: { type: String , required: true},
+  story: { type: String },
+  photo_url: { type: [String] },
+  charter: { type: Number },
+  street: { type: String },
+  city: { type: String },
+  state: { type: String },
+  zip: { type: Number },
+  country: { type: String },
+  coordinates: {
+    type: [Number], //latitude, longitude
+    index: '2d',
+    required: true
+  },
+  email : { 
+    type: String,
+    unique: true,
+  }, 
+  steward_name: { type: String },
+  status : { 
+    type: Boolean,
+    default: true 
+  }
+});
+
+module.exports = mongoose.model('Library', librarySchema);
---
+++ test/routes.js
@@ -0,0 +1,61 @@
+'use strict'
+
+import request from 'supertest'
+import {should} from 'chai'
+import config from '../config'
+import app from '../dist/server'
+
+const library = {
+  _id: 1,
+  name: 'Test Library',
+  story: 'Test Story',
+  coordinates: ['33.8121', '117.9190']
+}
+
+describe('Routes test', () => {
+  it('POST should return 405 error', done => {
+    request(app)
+      .post('/libraries/1')
+      .send(JSON.stringify(library))
+      .set('Accept', 'application/json')
+      .expect(405)
+      .end((err, res) => {
+        res.status.should.equal(405)
+        done()
+      })
+  })
+
+  it('PUT should return 405 code', done => {
+    request(app)
+      .put('/libraries/1')
+      .send(JSON.stringify(library))
+      .set('Accept', 'application/json')
+      .expect(405)
+      .end((err, res) => {
+        res.status.should.equal(405)
+        done()
+      })
+  })
+  
+  it('PATCH should return 405 code', done => {
+    request(app)
+      .patch('/libraries/1')
+      .send(JSON.stringify(library))
+      .set('Accept', 'application/json')
+      .expect(405)
+      .end((err, res) => {
+        res.status.should.equal(405)
+        done()
+      })
+  })
+
+  it('DELETE should return 405 code', done => {
+    request(app)
+      .delete('/libraries/1')
+      .expect(405)
+      .end((err, res) => {
+        res.status.should.equal(405)
+        done()
+      })
+  })
+})
